8d76c6182096ea397458666dc5c45b17b5a74400
LopesJosiel <josielslopes@gmail.com>
2023-06-17 19:55:05 -0300

    Projeto finalizado

--- GeradorApostasJava/src/EntradaDados.java
+++ GeradorApostasJava/src/EntradaDados.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random; // importando a classe random
 import java.util.Scanner;
 public class EntradaDados {
     public static void main (String [] args){
@@ -5,9 +8,16 @@ public class EntradaDados {
         int n1;
         int n2;
         System.out.println ("Informe a quantidade de números possíveis:\n");
+        // Lendo a entrada no teclado do total de números possíveis;
         n1 = ler.nextInt();
         ler.nextLine(); // esvazia o buffer do teclado
         System.out.println ("Informe quantos números vai apostar:\n");
         n2 = ler.nextInt();
+        //utilizando a classe par trabalhar com números aleatórios
+        Random aposta = new Random();
+        for (int i = 1; i < n2+1; i++) {
+            System.out.println(aposta.nextInt(n1));
+        }
+
     }
 }
